8242e727e9a58a3cf3d1b4f2480e005b68249a3e
Adedotun Adepoju <adedotun.adepoju@b54.co>
2022-10-22 20:27:59 +0100

    add selenium to requirements

--- airflow/web_scraping.py
+++ airflow/web_scraping.py
@@ -1,8 +1,10 @@
+import os
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-# url for the teams id
-api_url = "https://dashboard.api-football.com/soccer/ids/teams/England"
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # url to get PL team names 
 standings_url = "https://www.espn.com/soccer/standings/_/league/eng.1"
@@ -10,12 +12,29 @@ standings_url = "https://www.espn.com/soccer/standings/_/league/eng.1"
 # standings
 standings_page = urlopen(standings_url)
 standings_html = standings_page.read().decode("utf-8")
-standings_soup = BeautifulSoup(html, "html.parser")
+standings_soup = BeautifulSoup(standings_html, "html.parser")
 
-soup.findall("div", class_="team_link")
+results = standings_soup.find_all("div" , class_="team-link")
 
+current_clubs = []
+
+for result in results:
+    club_links = result.find_all("a")
+    club = club_links[2].text
+    clubs.append(club)
+
+print(clubs)
 
 # api
+
+# url for the teams id
+api_url = "https://dashboard.api-football.com/soccer/ids/teams/England"
+
 api_page = urlopen(api_url)
 api_html = api_page.read().decode("utf-8")
-api_soup = BeautifulSoup(html, "html.parser")
+api_soup = BeautifulSoup(api_html, "html.parser")
+
+email = os.environ.get('EMAIL')
+password = os.environ.get("PASSWORD")
+
+print(email, password)
