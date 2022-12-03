bea5d8fb5257f11eb6fb730620fa732f6f3c1acf
d.e.adepoju@gmail.com <d.e.adepoju@gmail.com>
2022-12-03 14:38:53 +0000

    Fix bug

--- dbt/models/staging/cards.sql
+++ dbt/models/staging/cards.sql
@@ -1,6 +1,7 @@
 {{ config(materialized='table') }}
 
+
 SELECT DISTINCT *
-FROM {{ source('staging', 'cards_stats_table') }} c LEFT JOIN {{ ref('pl_teams_id' )}} pl
+FROM {{ source('staging', 'cards_stats_table') }} c LEFT JOIN {{ ref('pl_teams_id') }} pl
     ON c.team_id = pl.id 
 WHERE created_at = (SELECT MAX(created_at) FROM {{ source('staging', 'cards_stats_table') }})
--- dbt/models/staging/games.sql
+++ dbt/models/staging/games.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
 SELECT DISTINCT *
-FROM {{ source('staging', 'game_stats_table') }} g LEFT JOIN {{ ref('pl_teams_id' )}} pl
+FROM {{ source('staging', 'game_stats_table') }} g LEFT JOIN {{ ref('pl_teams_id') }} pl
     ON g.team_id = pl.id 
 WHERE created_at = (SELECT MAX(created_at) FROM {{ source('staging', 'game_stats_table') }})
